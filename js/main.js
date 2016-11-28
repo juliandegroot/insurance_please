ba98df434c2f1af5a58035e65dbb08cbc1ea832a
Julian de Groot <j.groot@rgomiddelharnis.nl>
2016-11-28 23:55:44 +0100

    Concept

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-//initiate the Phaser framework
-var game = new Phaser.Game(360, 640, Phaser.AUTO);
+var InsurancePlz = InsurancePlz || {};
 
-game.state.add('GameState', GameState);
-game.state.add('HomeState', HomeState);
-game.state.add('PreloadState', PreloadState);
-game.state.add('BootState', BootState);
-game.state.start('BootState');
+InsurancePlz.game = new Phaser.Game(896, 504, Phaser.AUTO);
+//attackmap is 640x405
+//attackpanel is 640x99
+//newspanel is 256x504
+
+InsurancePlz.game.state.add('Boot', InsurancePlz.BootState);
+InsurancePlz.game.state.add('Preload', InsurancePlz.PreloadState);
+InsurancePlz.game.state.add('Game', InsurancePlz.GameState);
+
+InsurancePlz.game.state.start('Boot');
