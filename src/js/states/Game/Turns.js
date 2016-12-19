baa3331c708113ccb7f5ffc6dac165f10f30b243
Julian de Groot <j.groot@rgomiddelharnis.nl>
2016-12-19 22:31:11 +0100

    Damage animation fix, startturn delay

--- src/js/states/Game/Turns.js
+++ src/js/states/Game/Turns.js
@@ -1,4 +1,4 @@
-InsurancePlz.GameState.startTurn = function() {
+InsurancePlz.GameState.startTurn = function () {
     //Pop up news message, fade out & make uninteractable rest of game
     if (this.gameProgress.newsarray === undefined || this.gameProgress.newsarray.length == 0) {
         this.popup = new Popup("Game Start", "There is no news today!\nHappy hacking!\nRemember.. if you are looking for some sort of manual on how to play, click on the big information icon on the lower right\nAll hackers start out gathering information..", 'popuppanel');
@@ -7,7 +7,7 @@ InsurancePlz.GameState.startTurn = function() {
         var text = "THE NEWS\n";
         for (var i = 0; i < this.gameProgress.newsarray.length; i++) {
             var newsitem = this.gameProgress.newsarray[i];
-            text += newsitem.body + "\n\n";
+            text += newsitem.body + "\n";
         }
         this.popup = new Popup("Turn " + this.gameProgress.turn + " News", text, 'popuppanel');
         this.popup.addButton("Close", this.closePopup, this);
@@ -20,8 +20,8 @@ InsurancePlz.GameState.endTurn = function() {
     this.executeAttacks() // we are executing our attacks
     this.clearAttackStack(); // clear stacked attack array
     this.setAllButtonxAvailable(); // all button positions can be taken again
-    //this.triggerMajorEvent();
-    //this.triggerMinorEvent();
+    this.triggerMajorEvent();
+    this.triggerMinorEvent();
     this.gameProgress.index = 0;
     this.gameProgress.turn++;
     this.gameProgress.actionPoints = this.gameProgress.actionPointsMax;
@@ -30,6 +30,7 @@ InsurancePlz.GameState.endTurn = function() {
     if (this.gameProgress.turn > 10) {
         this.endGame();
     } else {
+    startruntimer = this.game.time.events.add(Phaser.Timer.SECOND * 2, this.startTurn, this);
         //this.startTurn();
     }
 };
