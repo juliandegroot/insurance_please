6dcc8e6c1f86de068c5f334264dcef30962d9712
Julian de Groot <j.groot@rgomiddelharnis.nl>
2016-12-21 22:36:11 +0100

    Improved on testing feedback
    attackweights added as visual (based on testing feedback)
    majorevents description clarified (based on testing feedback
    targetnames adjusted able to wordwrapped
    targetnames added as visual (based on testing feedback)
    included modal.js to create modal windows
    updated maximum attackpoints to meet tester criteria
    attackstacking failures visualized with modal windows
    targetname added on stacked attack

--- src/js/states/Game/Init.js
+++ src/js/states/Game/Init.js
@@ -34,7 +34,7 @@ InsurancePlz.GameState.createTargetData = function(attackMapData, targetData) {
  * createMap creates the map part of the game: it reads in the targets, attacks
  * and assigns them to a possition in the UI.
  */
-InsurancePlz.GameState.createMap = function() {
+InsurancePlz.GameState.createMap = function () {
     this.attackMapData = JSON.parse(this.game.cache.getText(this.playerData.attackmap));
     var targets = this.createTargetData(this.attackMapData, JSON.parse(this.game.cache.getText('targets')).targets);
 
@@ -44,18 +44,44 @@ InsurancePlz.GameState.createMap = function() {
     //create target instances
     this.targets = this.add.group();
     var target;
-    targets.forEach(function(targetData) {
+    targets.forEach(function (targetData) {
         target = new InsurancePlz.Target(this, targetData);
+        
+        var stylif = {
+            font: "350px Arial",
+            fill: "#ffff00",
+            align: 'left',
+            wordWrap: true,
+            wordWrapWidth: 50
+        };
+        var label_score = this.game.add.text(-50, 0, targetData.name, stylif); // adding attackweights/points as labels above the sprite
+        label_score.stroke = '#000000'; // give it a black outline (stroke)
+        label_score.fontSize = 350;
+        label_score.strokeThickness = 3;
+        target.addChild(label_score); // add it as child to the original instance
+        this.game.world.bringToTop(label_score); // put it on top of the actual sprite
+    
         this.targets.add(target);
     }, this);
 
     //create attack instances
     this.attacks = this.add.group();
     var attack;
-    this.attackMapData.attacks.forEach(function(attackData) {
+    this.attackMapData.attacks.forEach(function (attackData) {
         attack = new InsurancePlz.Attack(this, attackData);
+
+        var stylie = {
+            font: "35px Arial",
+            fill: "#ff0000"
+        };
+        var label_score = this.game.add.text(10, -30, attackData.points + " points", stylie); // adding attackweights/points as labels above the sprite
+        label_score.stroke = '#000000'; // give it a black outline (stroke)
+        label_score.strokeThickness = 6;
+        attack.addChild(label_score); // add it as child to the original instance
+        this.game.world.bringToTop(label_score); // put it on top of the actual sprite
         this.attacks.add(attack);
     }, this);
+
 };
 
 
@@ -67,4 +93,58 @@ InsurancePlz.GameState.createMap = function() {
 InsurancePlz.GameState.showHowToPlay = function() {
     this.popup = new Popup("Hackers Manual", "Companies are able to secure themselves in various ways by taking security measures:\n1. IoT protected devices: provides strong protection against all except password stealing and social engineering.\n2. No BYOD-policy: provides strong protection against all except password stealing and social engineering.\n3. Password management: provides strong protection against password stealing and social engineering.\n4. Email or web security devices: provides strong protection against all except usb-leaks, password stealing and social engineering..\n5. Auto-software-update on all devices: provides strong protection against almost every attack except password stealing and social engineering.\n6. Firewall updating: provides strong protection against almost every attack except password stealing and social engineering.\n7. Staff training (cyberaware): protects mildly against all attacks.\n8. Risk assessment & audit: protects mildly against all attacks.\n9. Technical advice on hard- and software risks: protects mildly against all attacks.\n10. Service contract (SLA) with IT-specialists: protects mildly against all attacks. ", 'howtoplaypanel');
     this.popup.addButton("Let's hack", this.closePopup, this);
+};
+
+
+
+InsurancePlz.GameState.createModals = function() {
+    //////// modal 1 ////////////
+    reg.modal.createModal({
+        type: "modal1",
+        includeBackground: true,
+        modalCloseOnInput: true,
+        itemsArr: [{
+            type: "graphics",
+            graphicColor: "0xffffff",
+            graphicWidth: 300,
+            graphicHeight: 300,
+            graphicRadius: 40
+        }, {
+            type: "text",
+            content: "Not enough attack points",
+            fontFamily: "Luckiest Guy",
+            fontSize: 22,
+            color: "0x1e1e1e",
+            offsetY: -50
+        }, ]
+    });
+    //////// modal 2 ////////////
+    reg.modal.createModal({
+        type: "modal2",
+        includeBackground: true,
+        modalCloseOnInput: true,
+        itemsArr: [{
+            type: "graphics",
+            graphicColor: "0xffffff",
+            graphicWidth: 300,
+            graphicHeight: 300,
+            graphicRadius: 40
+        }, {
+            type: "text",
+            content: "Already stacked for target",
+            fontFamily: "Luckiest Guy",
+            fontSize: 22,
+            color: "0x1e1e1e",
+            offsetY: -50
+        }, ]
+    });
+};
+
+InsurancePlz.GameState.showModalNotEnoughAttackPoints = function() {
+    reg.modal.showModal("modal1");
+};
+
+
+InsurancePlz.GameState.showModalAlreadyStacketforTarget = function() {
+    reg.modal.showModal("modal2");
 };
