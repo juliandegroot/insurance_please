46e0d84e3ba9ee4ec609fad9bdbdfe71f38b2ad4
Tom Peeters <tompeeters368@gmail.com>
2016-12-23 14:27:43 +0100

    Added global security vulnerability events.

--- src/js/states/Game/MajorEvents.js
+++ src/js/states/Game/MajorEvents.js
@@ -26,31 +26,32 @@ InsurancePlz.GameState.triggerMajorEvent = function() {
     }
 };
 
-InsurancePlz.GameState.triggerMajorEvent_global_nobyod = function() {
+//The following events trigger global security upgrades for a certain security key.
+InsurancePlz.GameState.triggerMajorEvent_global_sec_nobyod = function() {
     this.globalSecurityUpgrade("nobyod");
 };
-InsurancePlz.GameState.triggerMajorEvent_global_pwman = function() {
+InsurancePlz.GameState.triggerMajorEvent_global_sec_pwman = function() {
     this.globalSecurityUpgrade("pwman");
 };
-InsurancePlz.GameState.triggerMajorEvent_global_websec = function() {
+InsurancePlz.GameState.triggerMajorEvent_global_sec_websec = function() {
     this.globalSecurityUpgrade("websec");
 };
-InsurancePlz.GameState.triggerMajorEvent_global_autoup = function() {
+InsurancePlz.GameState.triggerMajorEvent_global_sec_autoup = function() {
     this.globalSecurityUpgrade("autoup");
 };
-InsurancePlz.GameState.triggerMajorEvent_global_firewall = function() {
+InsurancePlz.GameState.triggerMajorEvent_global_sec_firewall = function() {
     this.globalSecurityUpgrade("firewall");
 };
-InsurancePlz.GameState.triggerMajorEvent_global_stafftrain = function() {
+InsurancePlz.GameState.triggerMajorEvent_global_sec_stafftrain = function() {
     this.globalSecurityUpgrade("stafftrain");
 };
-InsurancePlz.GameState.triggerMajorEvent_global_riskaudit = function() {
+InsurancePlz.GameState.triggerMajorEvent_global_sec_riskaudit = function() {
     this.globalSecurityUpgrade("riskaudit");
 };
-InsurancePlz.GameState.triggerMajorEvent_global_techadvice = function() {
+InsurancePlz.GameState.triggerMajorEvent_global_sec_techadvice = function() {
     this.globalSecurityUpgrade("techadvice");
 };
-InsurancePlz.GameState.triggerMajorEvent_global_servicecon = function() {
+InsurancePlz.GameState.triggerMajorEvent_global_sec_servicecon = function() {
     this.globalSecurityUpgrade("servicecon");
 };
 
@@ -60,5 +61,43 @@ InsurancePlz.GameState.triggerMajorEvent_global_servicecon = function() {
 InsurancePlz.GameState.globalSecurityUpgrade = function(key) {
     for (var i = 0; i < this.targets.children.length; i++) {
         this.targets.children[i].data.securityVector[key] = 1;
+    }
+};
+
+//The following events trigger global security vulnerabilities for a certain security key.
+InsurancePlz.GameState.triggerMajorEvent_global_vuln_nobyod = function() {
+    this.globalSecurityDowngrade("nobyod");
+};
+InsurancePlz.GameState.triggerMajorEvent_global_vuln_pwman = function() {
+    this.globalSecurityDowngrade("pwman");
+};
+InsurancePlz.GameState.triggerMajorEvent_global_vuln_websec = function() {
+    this.globalSecurityDowngrade("websec");
+};
+InsurancePlz.GameState.triggerMajorEvent_global_vuln_autoup = function() {
+    this.globalSecurityDowngrade("autoup");
+};
+InsurancePlz.GameState.triggerMajorEvent_global_vuln_firewall = function() {
+    this.globalSecurityDowngrade("firewall");
+};
+InsurancePlz.GameState.triggerMajorEvent_global_vuln_stafftrain = function() {
+    this.globalSecurityDowngrade("stafftrain");
+};
+InsurancePlz.GameState.triggerMajorEvent_global_vuln_riskaudit = function() {
+    this.globalSecurityDowngrade("riskaudit");
+};
+InsurancePlz.GameState.triggerMajorEvent_global_vuln_techadvice = function() {
+    this.globalSecurityDowngrade("techadvice");
+};
+InsurancePlz.GameState.triggerMajorEvent_global_vuln_servicecon = function() {
+    this.globalSecurityDowngrade("servicecon");
+};
+
+/**
+ * Sets the security level of the given key for every target to 0 (minimum).
+ */
+InsurancePlz.GameState.globalSecurityDowngrade = function(key) {
+    for (var i = 0; i < this.targets.children.length; i++) {
+        this.targets.children[i].data.securityVector[key] = 0;
     }
 };
