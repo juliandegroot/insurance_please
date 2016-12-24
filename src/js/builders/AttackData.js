ea22f26ad7c70d71d5312ac6cc6c5594f041a0e3
Tom Peeters <tompeeters368@gmail.com>
2016-12-24 19:57:38 +0100

    Moved the buildAttackDataFromJSON into AttackData to match minor/majorEvent

--- src/js/builders/AttackData.js
+++ src/js/builders/AttackData.js
@@ -14,3 +14,18 @@ InsurancePlz.AttackData.prototype.constructor = InsurancePlz.AttackData;
 InsurancePlz.AttackData.prototype.updateSecurityVector = function(securityVector) {
     this.securityVector = securityVector;
 };
+
+/**
+ * Creates an array of attack objects which it parses from the provided JSON
+ * String.
+ * @param {String} attacksJSON A string in JSON format containing all attacks.
+ * @return {Array} attacks An array containing all the attacks in the JSON string.
+ */
+function createAttacksFromJSON(attacksJSON) {
+    var list = JSON.parse(attacksJSON).attacks;
+    var attacks = [];
+    for (i = 0; i < list.length; i++) {
+        attacks.push(new InsurancePlz.AttackData(list[i].attackName, list[i].securityVector, list[i].attackImage));
+    }
+    return attacks;
+}
